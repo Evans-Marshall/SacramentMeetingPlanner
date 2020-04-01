85b253faff50454f68576c2611a4adc06ba34c6e
Steve <stephenjcostigan@hotmail.com>
2020-03-31 22:28:05 -0500

    Added Scaffolding, Foreach loops for elements, and created element object inside of meetings.

--- Models/Meeting.cs
+++ Models/Meeting.cs
@@ -20,6 +20,8 @@ namespace SacramentMeetingPlanner.Models
         public int ClosingHymnNumber { get; set; }
         public string ClosingHymn { get; set; }
         public string ClosingPrayer { get; set; }
-        
+
+        public ICollection<Elements> Elements { get; set; }
+
     }
 }
---
+++ Pages/Meetings/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SacramentMeetingPlanner.Data;
+using SacramentMeetingPlanner.Models;
+
+namespace SacramentMeetingPlanner.Pages.Meetings
+{
+    public class EditModel : PageModel
+    {
+        private readonly SacramentMeetingPlanner.Data.SacramentMeetingPlannerContext _context;
+
+        public EditModel(SacramentMeetingPlanner.Data.SacramentMeetingPlannerContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Meeting Meeting { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Meeting = await _context.Meeting.FirstOrDefaultAsync(m => m.MeetingID == id);
+
+            if (Meeting == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Meeting).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MeetingExists(Meeting.MeetingID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MeetingExists(Guid id)
+        {
+            return _context.Meeting.Any(e => e.MeetingID == id);
+        }
+    }
+}
