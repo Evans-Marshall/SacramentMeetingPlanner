eee45563b61ac747afe8c4f3e61395245f8740c5
Joseph Wurz <wur17002@byui.edu>
2020-03-31 21:48:56 -0500

    initial commit w/  model

---
+++ Models/Elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Elements
+    {
+        public Guid ID { get; set; }
+        public string MeetingId { get; set; }
+        public string Type { get; set; }
+        
+        public string Assignment { get; set; }
+        public int HymnNumber { get; set; }
+        
+    }
+}
---
+++ Models/Meeting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Meeting
+    {
+        public Guid MeetingID { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string Conducting { get; set; }
+        public int OpeningHymnNumber { get; set; }
+        public string OpeningPrayer { get; set; }
+        public string OpeningHymn { get; set; }
+        public int SacramentHymnNumber { get; set; }
+        public string SacramentHymn { get; set; }
+        public int ClosingHymnNumber { get; set; }
+        public string ClosingHymn { get; set; }
+        public string ClosingPrayer { get; set; }
+        
+    }
+}
