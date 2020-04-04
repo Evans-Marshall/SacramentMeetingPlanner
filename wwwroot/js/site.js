7f1bee510b464eb2a2454b95c7be89c08c5ef67a
Steve <stephenjcostigan@hotmail.com>
2020-04-03 21:35:16 -0500

    Cleaned up index, added button for navigation, added background image, modified CSS, added JSON auto population for hymns

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function hymn(button) {
+    var h = document.getElementById(button.id).value;
+    var input = "hymn" + button.id;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://raw.githubusercontent.com/pseudosavant/LDSHymns/master/hymns.json", true);
+    xhr.send();
+
+    xhr.addEventListener("readystatechange", processRequest, false);
+
+    function processRequest(x) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var notify = JSON.parse(xhr.responseText);
+            document.getElementById(input).value = notify[h].name;
+
+        }
+    }
+}
