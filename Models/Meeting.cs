22cc6576257acb59b90d42291a493340084e4a19
Kirk Mortenson <46416608+kmortenson@users.noreply.github.com>
2020-04-04 19:30:30 -0700

    Validation

--- Models/Meeting.cs
+++ Models/Meeting.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,11 +13,18 @@ namespace SacramentMeetingPlanner.Models
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public string Conducting { get; set; }
+        [Range(1, 341)]
+        [Column(TypeName = "integer")]
         public int OpeningHymnNumber { get; set; }
+        [Range(1, 341)]
+        [Column(TypeName = "integer")]
         public string OpeningPrayer { get; set; }
         public string OpeningHymn { get; set; }
+        [Range(1, 341)]
         public int SacramentHymnNumber { get; set; }
         public string SacramentHymn { get; set; }
+        [Range(1, 341)]
+        [Column(TypeName = "integer")]
         public int ClosingHymnNumber { get; set; }
         public string ClosingHymn { get; set; }
         public string ClosingPrayer { get; set; }
@@ -24,6 +32,8 @@ namespace SacramentMeetingPlanner.Models
         //OPTIONAL FIELDS
         public string FirstSpeaker { get; set; }
         public string SecondSpeaker { get; set; }
+        [Range(1, 341)]
+        [Column(TypeName = "integer")]
         public int IntermediateHymnNumber { get; set; }
         public string IntermediateHymn { get; set; }
     }
